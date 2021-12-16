4043f3ff0bda6109fbf43fe3f2a1f744585a65e4
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-16 19:53:11 +0200

    додає рішення першої задачі

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+
+const categoryListElem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryListElem.length}`);
+categoryListElem.forEach(list => {
+    console.log(`Category: ${list.children[0].textContent}`);
+    console.log(`Elements: ${list.children[1].children.length}`);
+})
