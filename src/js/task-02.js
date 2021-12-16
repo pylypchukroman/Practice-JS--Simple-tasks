bf0da444dcebec4f3bbb5a69b6582c82f7b3f196
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-16 22:17:58 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,9 +7,16 @@ const ingredients = [
   'Condiments',
 ];
 
-  for (const ingredient of ingredients) {
-  const itemElem = document.createElement('li')
-  itemElem.classList.add('item');
-  itemElem.textContent = ingredient;
-  console.log(itemElem);
+const ingredientElem = ingredients.map(
+  ingredient => {
+    const itemElem = document.createElement('li');
+    itemElem.classList.add('item');
+    itemElem.textContent = ingredient;
+    return itemElem;
   }
+);
+
+const listElem = document.querySelector('#ingredients');
+console.log(listElem);
+
+listElem.append(...ingredientElem);
