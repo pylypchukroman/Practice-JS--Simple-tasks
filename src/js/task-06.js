0b4d9d664c805a22479408492a17b127af7c8a5c
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-18 10:57:18 +0200

    додає рішення задачі 6

---
+++ src/js/task-06.js
@@ -0,0 +1,16 @@
+const inputElem = document.querySelector('#validation-input');
+const atributElem = inputElem.getAttribute('data-length');
+// console.log(atributElem);
+
+inputElem.addEventListener("blur", () => {
+
+    if (inputElem.value.length == atributElem) {
+
+        inputElem.classList.add('valid');
+        inputElem.classList.remove('invalid');
+        return;
+    }
+    inputElem.classList.add('invalid');
+    inputElem.classList.remove('valid');
+
+});
