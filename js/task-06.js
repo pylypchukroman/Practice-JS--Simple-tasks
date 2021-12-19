61709a97eb2a26a9894bd7d1d371a598d8ca6513
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-20 00:54:14 +0200

    фіксить задачу 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,31 @@
 const inputElem = document.querySelector('#validation-input');
 const atributElem = inputElem.getAttribute('data-length');
+const totallenght = parseInt(atributElem, 10);
+
+const classes = function (clas1, clas2) {
+    inputElem.classList.add(clas1);
+    inputElem.classList.remove(clas2);
+}
 
 inputElem.addEventListener("blur", () => {
 
-    if (inputElem.value.length == atributElem) {
-        inputElem.classList.add('valid');
-        inputElem.classList.remove('invalid');
+    if (inputElem.value.length === totallenght) {
+        classes('valid', 'invalid');
         return;
     }
-    inputElem.classList.add('invalid');
-    inputElem.classList.remove('valid');
+    classes('invalid', 'valid');
 
 });
+
+
+// inputElem.addEventListener("blur", () => {
+
+//     if (inputElem.value.length == atributElem) {
+//         inputElem.classList.add('valid');
+//         inputElem.classList.remove('invalid');
+//         return;
+//     }
+//     inputElem.classList.add('invalid');
+//     inputElem.classList.remove('valid');
+
+// });
