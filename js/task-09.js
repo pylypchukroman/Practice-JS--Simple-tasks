7cfd19d0e412b521fc591fcea0496629a085fe0f
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-20 17:39:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -7,3 +7,10 @@ buttonElem.addEventListener('click', function () {
   const changeColor = document.body.style.backgroundColor = getRandomHexColor();
   colorElem.textContent = changeColor;
 })
+
+// const buttonElem = document.querySelector('.change-color');
+// const colorElem = document.querySelector('.color');
+// buttonElem.addEventListener('click', function () {
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   colorElem.textContent = document.body.style.backgroundColor;
+// })
